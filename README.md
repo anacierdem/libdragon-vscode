bd0fd3abc611b31a02da16a34271b193b894b7e7
Ali Naci Erdem <anacierdem@gmail.com>
2023-12-02 10:37:52 +0300

    Update grammar and initial attempt at stall detection

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 A vscode extension to simplify development with `libdragon`.
 
+Currently supported features:
+
+- RSP MIPS assembly highlighting (WIP)
+- RSP MIPS assembly pipeline stall detection (WIP)
+
 ### TODO:
 
 - [ ] Implement preview for libdragon sprites
-- [ ] Add more descriptive highlighting for known as directives
 - [ ] Extend syntax highlighting to also cover regular MIPS assembly files
+- [ ] Add more descriptive highlighting for known as directives
 - [ ] Better handling of C preprocessor directives for MIPS assembly highlighting
 - [ ] Implement semantic MIPS assembly highlighting. Current version is all static
--- src/extension.ts
+++ src/extension.ts
@@ -9,11 +9,26 @@ import * as vsctm from "vscode-textmate";
 // import * as oniguruma from "vscode-oniguruma";
 const oniguruma = require("vscode-oniguruma");
 
+type Operand = {
+  name: string;
+  range: vscode.Range;
+};
+
+type Instruction = {
+  name: string;
+  operands: (Operand | undefined)[];
+  range: vscode.Range;
+  lineRange: vscode.Range;
+};
+
 function readFile(path: string): Promise<Buffer> {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (error, data) => (error ? reject(error) : resolve(data)));
   });
 }
+
+// TODO: optimize this
+const MAX_LOOKBEHIND = 100;
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("Activated");
@@ -94,7 +109,109 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions,
   );
 
+  function getTarget(instruction: Instruction) {
+    switch (instruction.name) {
+      default:
+        return instruction.operands[0];
+    }
+  }
+
+  function getSources(instruction: Instruction) {
+    switch (instruction.name) {
+      default:
+        return instruction.operands.slice(1);
+    }
+  }
+
+  let dualIssued = false;
+  function getCycles(instructions: Instruction[]) {
+    const lastInstruction = instructions[instructions.length - 1];
+    const prevInstruction = instructions[instructions.length - 2];
+
+    if (dualIssued) {
+      dualIssued = false;
+      return 0;
+    }
+
+    if (
+      lastInstruction.name.startsWith("v") &&
+      !prevInstruction?.name.startsWith("v")
+    ) {
+      dualIssued = true;
+      return 0;
+    }
+
+    if (
+      !lastInstruction.name.startsWith("v") &&
+      prevInstruction?.name.startsWith("v")
+    ) {
+      dualIssued = true;
+      return 0;
+    }
+
+    // TODO: handle other cases
+    return 1;
+  }
+
+  function getLatency(instruction: Instruction) {
+    if (instruction.name?.startsWith("v")) {
+      return 4;
+    }
+    return 1;
+  }
+
+  const lineDecorations: vscode.DecorationOptions[] = [];
+  const wordDecorations: vscode.DecorationOptions[] = [];
+  const prevInstructions: Instruction[] = [];
+  async function analyzeInstruction(currentInstruction: Instruction) {
+    const len = prevInstructions.length;
+    const finalInstructions = [...prevInstructions, currentInstruction];
+
+    let cycleDiff = getCycles(finalInstructions);
+    for (let i = 0; i < len; i++) {
+      const instructionToTest = prevInstructions[len - i - 1];
+      const target = getTarget(instructionToTest);
+
+      const latencyDiff = getLatency(instructionToTest) - cycleDiff;
+
+      if (
+        target &&
+        getSources(currentInstruction)
+          .filter<Operand>((f): f is Operand => !!f)
+          .find(
+            ({ name }) => target.name.split(".")[0] === name.split(".")[0],
+          ) &&
+        latencyDiff > 0
+      ) {
+        // Stalled
+        cycleDiff += latencyDiff;
+
+        if (
+          lineDecorations[lineDecorations.length - 1]?.range !==
+          currentInstruction.lineRange
+        ) {
+          lineDecorations.push({
+            range: currentInstruction.lineRange,
+          });
+        }
+        wordDecorations.push({
+          range: currentInstruction.range,
+        });
+      }
+
+      cycleDiff += getCycles(prevInstructions.slice(0, len - i));
+    }
+    prevInstructions.push(currentInstruction);
+
+    while (prevInstructions.length > MAX_LOOKBEHIND) {
+      prevInstructions.shift();
+    }
+  }
+
   async function triggerUpdateDecorations() {
+    prevInstructions.length = 0;
+    lineDecorations.length = 0;
+    wordDecorations.length = 0;
     if (activeEditor) {
       const grammar = await registry.loadGrammar("source.mips.rsp");
 
@@ -104,39 +221,59 @@ export function activate(context: vscode.ExtensionContext) {
 
       const text = activeEditor.document.getText().split("\n");
 
-      let matches = [];
-      let lines = [];
       let ruleStack = vsctm.INITIAL;
       for (let i = 0; i < text.length; i++) {
         const line = text[i];
         const lineTokens = grammar.tokenizeLine(line, ruleStack);
 
-        let found = false;
+        let operandIndex = 0;
+        let currentInstruction: Instruction | null = null;
         for (let j = 0; j < lineTokens.tokens.length; j++) {
           const token = lineTokens.tokens[j];
 
-          if (token.scopes.includes("constant.numeric")) {
-            matches.push({
-              range: new vscode.Range(
-                new vscode.Position(i, token.startIndex),
-                new vscode.Position(i, token.endIndex),
-              ),
-            });
+          if (token.scopes.includes("punctuation.terminator") || j == 0) {
+            currentInstruction && analyzeInstruction(currentInstruction);
 
-            found = true;
+            currentInstruction = null;
+            operandIndex = 0;
+            continue;
           }
+
+          if (token.scopes.includes("support.function.instruction")) {
+            currentInstruction = currentInstruction || {
+              name: line.substring(token.startIndex, token.endIndex),
+              operands: [],
+              range: new vscode.Range(i, token.startIndex, i, token.endIndex),
+              lineRange: new vscode.Range(i, text[i].length, i, text[i].length),
+            };
+            continue;
+          }
+
+          if (token.scopes.includes("punctuation.separator")) {
+            operandIndex++;
+            continue;
+          }
+
+          if (token.scopes.includes("support.variable")) {
+            if (!currentInstruction) {
+              continue;
+            }
+
+            currentInstruction.operands[operandIndex] = {
+              name: line.substring(token.startIndex, token.endIndex),
+              range: new vscode.Range(i, token.startIndex, i, token.endIndex),
+            };
+            continue;
+          }
+
+          // TODO: bail out if something else is found
         }
 
-        if (found) {
-          lines.push({
-            range: new vscode.Range(i, text[i].length, i, text[i].length),
-          });
-        }
         ruleStack = lineTokens.ruleStack;
       }
 
-      activeEditor.setDecorations(highlightDecoration, matches);
-      activeEditor.setDecorations(lineDecoration, lines);
+      activeEditor.setDecorations(lineDecoration, lineDecorations);
+      activeEditor.setDecorations(highlightDecoration, wordDecorations);
     }
   }
 }
