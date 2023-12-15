6e1952bc6beb3b83d5f2085c57cf865ba0ceb5be
Ali Naci Erdem <anacierdem@gmail.com>
2023-12-15 23:15:05 +0300

    Start supporting rsp and regular mips files

--- build/index.ts
+++ build/index.ts
@@ -3,41 +3,58 @@ import * as fs from 'fs';
 
 import { load } from 'js-yaml';
 
+const rspLanguage = load(
+  fs.readFileSync(
+    path.join(__dirname, "..", "syntaxes", "mips.rsp.tmLanguage.template.yaml"),
+    "utf8",
+  ),
+) as Record<string, any>;
 
-const inputLanguage = load(
-    fs.readFileSync(path.join(__dirname, '..', 'syntaxes', 'rsp.tmLanguage.template.yaml'), 'utf8')
-) as any;
-
-const variables = Object.assign({}, inputLanguage.variables);
 const regex = /\{\{(.*?)\}\}/gm;
-function substituteVariables(this: any, key: string, input: string) {
-    if(typeof input === 'string') {
-        console.log(`Substituting variables in ${key}: ${input}`)
-        let output = input;
-        let results;
-        do {
-            results = regex.exec(input);
-            if(results) {
-                for (let i = 1; i < results.length; i++) {
-                    const variableName = results[i];
-                    const variableValue = variables[variableName];
-                    console.log(`Substituting ${variableName} with ${variableValue}`);
-                    output = output.replace(new RegExp(`\\{\\{${variableName}\\}\\}`), variableValue);
-                }
-            }
-        } while(results);
-        // Mutate variables so that they can be used later
-        if (this === inputLanguage.variables) {
-            console.log(`Update vars ${key}: ${output}`)
-            variables[key] = output;
+const substituteVariables = (inputLanguage: Record<string, any>) => {
+  const variables = Object.assign({}, inputLanguage.variables);
+  return function (this: any, key: string, input: string) {
+    if (typeof input === "string") {
+      console.log(`Substituting variables in ${key}: ${input}`);
+      let output = input;
+      let results;
+      do {
+        results = regex.exec(input);
+        if (results) {
+          for (let i = 1; i < results.length; i++) {
+            const variableName = results[i];
+            const variableValue = variables[variableName];
+            console.log(`Substituting ${variableName} with ${variableValue}`);
+            output = output.replace(
+              new RegExp(`\\{\\{${variableName}\\}\\}`),
+              variableValue,
+            );
+          }
         }
-        return output.replace(/\s/g, "");
+      } while (results);
+      // Mutate variables so that they can be used later
+      if (this === inputLanguage.variables) {
+        console.log(`Update vars ${key}: ${output}`);
+        variables[key] = output;
+      }
+      return output.replace(/\s/g, "");
     } else {
-        return input;
+      return input;
     }
-}
+  };
+};
 
 fs.writeFileSync(
-    path.join(__dirname, '..', 'syntaxes', 'rsp.tmLanguage.json'),
-    JSON.stringify(inputLanguage, substituteVariables, '  ')
-)
+  path.join(__dirname, "..", "syntaxes", "mips.rsp.tmLanguage.json"),
+  JSON.stringify(rspLanguage, substituteVariables(rspLanguage), "  "),
+);
+
+// Renaming just for convenience, we will soon mutate the object
+const mipsLanguage = rspLanguage;
+delete mipsLanguage.repository.not_impl_inst;
+delete mipsLanguage.repository.instruction.captures;
+mipsLanguage.scopeName = "source.mips";
+fs.writeFileSync(
+  path.join(__dirname, "..", "syntaxes", "mips.tmLanguage.json"),
+  JSON.stringify(mipsLanguage, substituteVariables(mipsLanguage), "  "),
+);
